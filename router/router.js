d451ab62d59dc5fc5721998860603a9e647cc168
Himanshu Jasani <jasanihimanshu779@gmail.com>
2024-09-02 16:00:11 +0530

    router.js

--- router/router.js
+++ router/router.js
@@ -1,41 +1,16 @@
-const express = require('express')
-const router = express();
-const session = require('express-session');
+const express = require('express');
+const router = express.Router();
 const passport = require('passport');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const passportJWT = require('../auth/auth');
-// const port = 9000;
-// router.use(express.json());
-// router.use(bodyParser.json());
-// router.use(express.urlencoded());
-// router.use(cors());
 
-// mongoose.set('strictQuery', false);
-// const url = 'mongodb+srv://option:admin1234@cluster0.8mxyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-// mongoose.connect(url).then(()=>{
-//     console.log('Database connected');
-// }).catch((err)=>{
-//     console.log(err);
-// });
+const controller = require('../controllers/controllers');
 
-router.use(session({
-    secret: 'secretkey',
-    resave: false,
-    saveUninitialized: true,
-}))
-
-router.use(passport.initialize());
-router.use(passport.session());
-// app.use('/', require('../router/router'))
-
-// router.listen(port, (err) => {
-//     if(err)
-//     {
-//         console.log(err);
-//     }
-//     console.log(`Server is running on port ${port}`);
-// });
-
+router.post('/register', controller.register);
+router.post('/login', controller.login);
+router.get('/dashboard', passport.authenticate('jwt', { session: false }), controller.dashboard)
+router.get('/getallData', passport.authenticate('jwt', { session: false }), controller.getallData);
+router.post('/addData', passport.authenticate('jwt', { session: false }),controller.createData);
+router.put('/updateInventory/:id', passport.authenticate('jwt', { session: false }),controller.updateInventory);
+router.delete('/deleteInventory/:id', passport.authenticate('jwt', { session: false }),controller.deleteData);
+router.get('/getRantedInventory', passport.authenticate('jwt', { session: false }),controller.getRantedInventory);
+router.post('/invoices', controller.createInvoice);
 module.exports = router;
